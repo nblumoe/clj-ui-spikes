b64ad6505876b16f6aacd95aac9100c0f90344b9
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2017-01-16 09:53:21 +0100

    Open file dialog on button click

--- javafx-ui/src/javafxui/core.clj
+++ javafx-ui/src/javafxui/core.clj
@@ -1,14 +1,29 @@
 (ns javafxui.core
   (:import [javafx.application Application]
            [javafx.scene Scene]
-           [javafx.scene.control Button])
+           [javafx.scene.control Button]
+           [javafx.stage FileChooser])
   (:gen-class :extends javafx.application.Application))
 
-(defonce force-toolkit-init (javafx.embed.swing.JFXPanel.))
+
+(defn event-handler [f]
+  (reify javafx.event.EventHandler
+    (handle [this event] (f event))))
+
+(defn show-file-dialog [event]
+  (let [window (-> event
+                   .getTarget
+                   .getScene
+                   .getWindow)]
+    (doto (FileChooser.)
+      (.setTitle "Import CSV")
+      (.showOpenDialog window))))
 
 (defn -start [app stage]
   (let [button (Button. "Import CSV")
         scene (Scene. button)]
+    (doto button
+      (.setOnAction (event-handler show-file-dialog)))
     (doto stage
       (.setTitle "JavaFX UI Spike")
       (.setScene scene)
