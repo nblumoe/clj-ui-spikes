2246e05fcee513623377ac932b35e0361204ae9d
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2017-01-16 10:24:14 +0100

    Parse CSV data and store as runtime state

--- javafx-ui/project.clj
+++ javafx-ui/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.3"]]
   :main ^:skip-aot javafxui.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- javafx-ui/src/javafxui/core.clj
+++ javafx-ui/src/javafxui/core.clj
@@ -1,27 +1,42 @@
 (ns javafxui.core
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io  :as io])
   (:import [javafx.application Application]
            [javafx.scene Scene]
            [javafx.scene.control Button]
            [javafx.stage FileChooser])
   (:gen-class :extends javafx.application.Application))
 
+(defonce runtime-state (atom {:csv-file nil
+                              :data nil}))
+
+(defn watch-fn [key reference old-state new-state]
+  (println "-- Runtime State Changed from:")
+  (println old-state)
+  (println "-- to:")
+  (println new-state))
 
 (defn event-handler [f]
   (reify javafx.event.EventHandler
     (handle [this event] (f event))))
 
+(defn import-file! [file]
+  (with-open [reader (io/reader file)]
+    (swap! runtime-state assoc
+           :csv-file file
+           :data (csv/read-csv reader))))
+
 (defn show-file-dialog [event]
-  (let [window (-> event
-                   .getTarget
-                   .getScene
-                   .getWindow)]
-    (doto (FileChooser.)
-      (.setTitle "Import CSV")
-      (.showOpenDialog window))))
+  (let [window (-> event .getTarget .getScene .getWindow)
+        dialog (doto (FileChooser.)
+                 (.setTitle "Import CSV"))
+        file (.showOpenDialog dialog window)]
+    (import-file! file)))
 
 (defn -start [app stage]
   (let [button (Button. "Import CSV")
         scene (Scene. button)]
+    (add-watch runtime-state :main-watcher watch-fn)
     (doto button
       (.setOnAction (event-handler show-file-dialog)))
     (doto stage
